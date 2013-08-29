f286e22ca8ace89224819ad69f66e109bf19af07
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-08-29 19:37:40 +0000

    - Prepped for beta build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'resource_report';
-$app['version'] = '1.4.30';
+$app['version'] = '1.5.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
