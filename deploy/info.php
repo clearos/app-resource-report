1d99642c1a69069fd71bae22071c96c63b762406
pcbaldwin <pbaldwin@clearfoundation.com>
2015-01-16 15:27:42 -0500

    Changed subcategory

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'resource_report';
-$app['version'] = '2.0.5';
+$app['version'] = '2.0.14';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
@@ -19,7 +19,7 @@ $app['description'] = lang('resource_report_app_description');
 
 $app['name'] = lang('resource_report_app_name');
 $app['category'] = lang('base_category_reports');
-$app['subcategory'] = lang('base_category_system');
+$app['subcategory'] = lang('base_subcategory_performance_and_resources');
 
 /////////////////////////////////////////////////////////////////////////////
 // Packaging
--- packaging/app-resource-report.spec
+++ packaging/app-resource-report.spec
@@ -1,7 +1,7 @@
 
 Name: app-resource-report
 Epoch: 1
-Version: 2.0.5
+Version: 2.0.14
 Release: 1%{dist}
 Summary: Resource Report
 License: GPLv3
