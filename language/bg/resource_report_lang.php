459489c04871bb4bfac8042bd6162e8518aa9fa5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:58:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Докладът на ресурсите включва информация за товара, използването на паметта и стартираните процеси.';
+$lang['resource_report_app_name'] = 'Доклад Resource';
+$lang['resource_report_load_average'] = 'Заредете Средна';
+$lang['resource_report_number_of_processes'] = 'Брой на процеси';
