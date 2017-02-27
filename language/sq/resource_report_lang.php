b71d5a946fcd00583f86a05ca36e205fe7809caa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:57:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Raporti i Burimeve përfshin informacion mbi ngarkesës, përdorimin e kujtesës dhe proceset running.';
+$lang['resource_report_app_name'] = 'Burimeve Raporti';
+$lang['resource_report_load_average'] = 'Mesatarja e ngarkesës';
+$lang['resource_report_number_of_processes'] = 'Numri i proceseve';
