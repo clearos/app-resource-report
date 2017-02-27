6ebe449d8ad811880a0eb3117d689603dbb2e799
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:58:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Baliabideen Txostena karga, Memoria erabilera eta prozesuak exekutatzen buruzko informazioa biltzen du.';
+$lang['resource_report_app_name'] = 'Baliabideen txostena';
+$lang['resource_report_load_average'] = 'Kargaren batazbestekoa';
+$lang['resource_report_number_of_processes'] = 'Prozesuak kopurua';
