a18cc1d005b2c57dcf493e3a617278ea0b104a11
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:58:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Ռեսուրս զեկույցը ներառում է տեղեկատվություն բեռի, հիշողության օգտագործման եւ վազում գործընթացների.';
+$lang['resource_report_app_name'] = 'Resource հաշվետվություն';
+$lang['resource_report_load_average'] = 'Load Միջին';
+$lang['resource_report_number_of_processes'] = 'Միավորների գործընթացների';
