e069025e09323185c719a929357103a0fd83e9f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:58:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'রিসোর্স প্রতিবেদন লোড, মেমরি ব্যবহার এবং চলমান প্রসেস সম্পর্কে আলোচনা করা হয়েছে.';
+$lang['resource_report_app_name'] = 'রিসোর্স প্রতিবেদন';
+$lang['resource_report_load_average'] = 'লোড গড়';
+$lang['resource_report_number_of_processes'] = 'প্রসেসের সংখ্যা';
