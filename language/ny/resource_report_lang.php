e7df624d43f5cbd4ade4e611a13b4fb3d929e098
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:59:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Report Resource kumaphatikizapo zambiri pa katundu, wakagwiritsidwe kukumbukira ndi njira akuthamanga.';
+$lang['resource_report_app_name'] = 'Report Resource';
+$lang['resource_report_load_average'] = 'katundu Avereji';
+$lang['resource_report_number_of_processes'] = 'Number njira';
