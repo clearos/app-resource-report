df8d923107fc6cc7db98aaaed2355451423df172
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:59:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = '资源报告包含负载，内存使用和运行的进程的信息。';
+$lang['resource_report_app_name'] = '资源报告';
+$lang['resource_report_load_average'] = '平均负载';
+$lang['resource_report_number_of_processes'] = '进程数';
