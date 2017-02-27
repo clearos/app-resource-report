7df23fd06c9752f9fefe4f6f616fa5c9d8bcf06e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:57:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'ويتضمن التقرير معلومات عن الموارد الحمل، واستخدام الذاكرة والعمليات الجارية.';
+$lang['resource_report_app_name'] = 'تقرير الموارد';
+$lang['resource_report_load_average'] = 'متوسط ​​الحمل';
+$lang['resource_report_number_of_processes'] = 'عدد العمليات';
