d89eacff8f9b299acade5c28316299a6abd97b31
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:57:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Die verslag Resource sluit inligting oor lading, geheue gebruik en bestuur van prosesse.';
+$lang['resource_report_app_name'] = 'hulpbron Verslag';
+$lang['resource_report_load_average'] = 'vrag Gemiddeld';
+$lang['resource_report_number_of_processes'] = 'Aantal prosesse';
