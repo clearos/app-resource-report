93f69cb78d114424ba6f46286a93cca184a2fde2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:59:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'အဆိုပါအရင်းအမြစ်အစီရင်ခံစာဝန်, မှတ်ဉာဏ်အသုံးပြုမှုနှင့်ပြေးဖြစ်စဉ်များအပေါ်သတင်းအချက်အလက်များပါဝင်သည်။';
+$lang['resource_report_app_name'] = 'အရင်းအမြစ်အစီရင်ခံစာ';
+$lang['resource_report_load_average'] = 'load ပျမ်းမျှ';
+$lang['resource_report_number_of_processes'] = 'Processes အရေအတွက်';
