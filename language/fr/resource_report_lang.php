666ba2f3d58392caf69838f4ddf40658eee9eabb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:35:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Le rapport sur les ressources comprend des informations sur la charge, utilisation de la mémoire et des processus en cours d&#39;exécution.';
+$lang['resource_report_app_name'] = 'Rapport sur les ressources';
+$lang['resource_report_load_average'] = 'charge moyenne';
+$lang['resource_report_number_of_processes'] = 'Nombre de processus';
