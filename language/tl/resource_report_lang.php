cb2c1a8b18b83cdcaaee7445a8cc63fae202421f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:56:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Ang Resource Isumbong kinabibilangan ng mga impormasyon sa pag-load, memory paggamit at pagpapatakbo ng mga proseso.';
+$lang['resource_report_app_name'] = 'Resource Report';
+$lang['resource_report_load_average'] = 'Load Average';
+$lang['resource_report_number_of_processes'] = 'Bilang ng Proseso';
