a0b34b8d8288c064716e986d58cd977ecd1c59e3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:53:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'گزارش منابع شامل اطلاعات در مورد بار، استفاده از حافظه و فرایندهای در حال اجرا.';
+$lang['resource_report_app_name'] = 'گزارش منابع';
+$lang['resource_report_load_average'] = 'متوسط ​​بار';
+$lang['resource_report_number_of_processes'] = 'تعدادی از فرآیندهای';
