2cd739d51cedc069762e9627a762122add40dd4c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:53:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'ਸਰੋਤ ਦੀ ਰਿਪੋਰਟ ਲੋਡ, ਮੈਮੋਰੀ ਦੀ ਵਰਤੋ ਅਤੇ ਚੱਲ ਰਹੇ ਕਾਰਜ &#39;ਤੇ ਜਾਣਕਾਰੀ ਵੀ ਸ਼ਾਮਲ ਹੈ.';
+$lang['resource_report_app_name'] = 'ਸਰੋਤ ਦੀ ਰਿਪੋਰਟ';
+$lang['resource_report_load_average'] = 'ਲੋਡ ਔਸਤ';
+$lang['resource_report_number_of_processes'] = 'ਕਾਰਜ ਦੀ ਗਿਣਤੀ';
