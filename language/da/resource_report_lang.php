baba4a33607be26a87060618087b9b6707b2f81c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:32:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Ressource rapport indeholder oplysninger om belastning, hukommelsesforbrug og kørende processer.';
+$lang['resource_report_app_name'] = 'rapport Resource';
+$lang['resource_report_load_average'] = 'Load Gennemsnitlig';
+$lang['resource_report_number_of_processes'] = 'Antal processer';
