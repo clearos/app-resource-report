c1b9a2b2c68c1e229e05e68c71362f299d6a1a11
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:49:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'ಸಂಪನ್ಮೂಲ ವರದಿ ಲೋಡ್, ಮೆಮೊರಿ ಬಳಕೆ ಮತ್ತು ಚಲಿಸುತ್ತಿರುವ ಪ್ರಕ್ರಿಯೆಗಳ ಬಗೆಗಿನ ಮಾಹಿತಿಯನ್ನು ಒಳಗೊಂಡಿದೆ.';
+$lang['resource_report_app_name'] = 'ಸಂಪನ್ಮೂಲ ವರದಿ';
+$lang['resource_report_load_average'] = 'ಲೋಡ್ ಸರಾಸರಿ';
+$lang['resource_report_number_of_processes'] = 'ಪ್ರಕ್ರಿಯೆಗಳು ಸಂಖ್ಯೆ';
