dc8df41581b5161fdc03a92954f0279c67139aab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:54:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Отчет Ресурс содержит информацию о нагрузке, использовании памяти и запущенных процессов.';
+$lang['resource_report_app_name'] = 'Отчет ресурсов';
+$lang['resource_report_load_average'] = 'Load Average';
+$lang['resource_report_number_of_processes'] = 'Количество процессов';
