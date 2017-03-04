0e1eedc69071398f80ef75eb9c1890341244523a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:54:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'මෙම සම්පත් වාර්තාව බර, මතක භාවිතය සහ දැනට ක්රියාත්මක වන ක්රියාදාමයන් පිළිබඳ තොරතුරු ඇතුළත් වේ.';
+$lang['resource_report_app_name'] = 'සම්පත් වාර්තාව';
+$lang['resource_report_load_average'] = 'පැටවුම් සාමාන්යය';
+$lang['resource_report_number_of_processes'] = 'ක්රියාවලි ගණන';
