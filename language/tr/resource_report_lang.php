e84fb805ea8c15f1a34451879a62dc42965cdcbe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:56:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Kaynak Raporu yük, bellek kullanımı ve çalışan süreçleri hakkında bilgi içerir.';
+$lang['resource_report_app_name'] = 'kaynak Raporu';
+$lang['resource_report_load_average'] = 'yük Ortalama';
+$lang['resource_report_number_of_processes'] = 'Süreçlerin sayısı';
