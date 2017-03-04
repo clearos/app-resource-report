9fc2180f0b6633ddf1157c48d6c1305760f80af1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:37:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Η έκθεση Πόρων περιλαμβάνει πληροφορίες σχετικά με το φορτίο, τη χρήση της μνήμης και των διαδικασιών λειτουργίας.';
+$lang['resource_report_app_name'] = 'Έκθεση των πόρων';
+$lang['resource_report_load_average'] = 'Μέσο φορτίο';
+$lang['resource_report_number_of_processes'] = 'Αριθμός Διαδικασιών';
