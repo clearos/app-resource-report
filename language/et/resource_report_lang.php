b7aa7d06ef21d27c5221fe194f8b66e699a8f456
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:34:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Allika aruanne sisaldab teavet koormus, mälukasutust ja töötavad protsessid.';
+$lang['resource_report_app_name'] = 'Allika aruanne';
+$lang['resource_report_load_average'] = 'Keskmine koormus';
+$lang['resource_report_number_of_processes'] = 'Protsesside arv';
