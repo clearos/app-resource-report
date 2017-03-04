b1a3ee1f68830cf5502e2bc3fb9e979878ff3b09
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:47:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Ресурс туралы есеп жүктемесі, жадыны жұмсау мен жүгіру процестер туралы ақпаратты қамтиды.';
+$lang['resource_report_app_name'] = 'Ресурс туралы есеп';
+$lang['resource_report_load_average'] = 'Load орташа';
+$lang['resource_report_number_of_processes'] = 'Процестерді саны';
