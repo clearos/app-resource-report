e49ed96604c89d7047faccf76cdec789e5716132
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:52:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Ir-Rapport Riżorsi tinkludi informazzjoni dwar it-tagħbija, l-użu tal-memorja u l-proċessi running.';
+$lang['resource_report_app_name'] = 'Rapport tar-Riżorsi';
+$lang['resource_report_load_average'] = 'Medja tagħbija';
+$lang['resource_report_number_of_processes'] = 'Għadd ta &#39;Proċessi';
