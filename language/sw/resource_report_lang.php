2c5f462ab55a607918fae90b554abce4dcac8a01
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:55:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Ripoti ya Rasilimali ni pamoja na taarifa juu ya mzigo, matumizi ya kumbukumbu na taratibu mbio.';
+$lang['resource_report_app_name'] = 'Ripoti ya Rasilimali';
+$lang['resource_report_load_average'] = 'mzigo Wastani';
+$lang['resource_report_number_of_processes'] = 'Idadi ya michakato';
