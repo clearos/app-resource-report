a75f4d040f1d1998c35ade11a26a7236133cb648
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:50:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Et Resource fama includit notitia in pondere, et in memoria usu processus currit.';
+$lang['resource_report_app_name'] = 'resource Report';
+$lang['resource_report_load_average'] = 'load Average';
+$lang['resource_report_number_of_processes'] = 'Processus of';
