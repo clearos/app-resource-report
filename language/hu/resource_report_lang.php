b0f227275535b4942c49baadb232ae4b40a17f13
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:41:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Az erőforrás adatokat tartalmaz terhelés, a memória használat és a futó folyamatokat.';
+$lang['resource_report_app_name'] = 'erőforrás-jelentés';
+$lang['resource_report_load_average'] = 'Load Average';
+$lang['resource_report_number_of_processes'] = 'Folyamatok számát';
