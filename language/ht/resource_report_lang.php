3bcbccf3cad6b6980b1fd79aa9c4490e927143ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:39:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Rapò a Resous gen ladan enfòmasyon sou chaj, l &#39;memwa ak pwosesis kouri.';
+$lang['resource_report_app_name'] = 'Rapò resous';
+$lang['resource_report_load_average'] = 'chaj Mwayèn';
+$lang['resource_report_number_of_processes'] = 'Nombre de Pwosesis';
