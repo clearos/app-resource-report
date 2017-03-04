62a02352fba81618325e7a50107a2cbd6c866adf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:53:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Ressursrapporten inneholder informasjon om lasten, minnebruk og prosesser som kjører.';
+$lang['resource_report_app_name'] = 'Ressursrapporten';
+$lang['resource_report_load_average'] = 'Load Gjennomsnittlig';
+$lang['resource_report_number_of_processes'] = 'Antall Prosesser';
