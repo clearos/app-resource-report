61525a9131daf60d7116ab44dae802721b15db96
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:57:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Mae Adroddiad Adnoddau yn cynnwys gwybodaeth am y llwyth, chof chynefod a phrosesau rhedeg.';
+$lang['resource_report_app_name'] = 'Adroddiad adnoddau';
+$lang['resource_report_load_average'] = 'Cyfartaledd llwytho';
+$lang['resource_report_number_of_processes'] = 'Nifer o Brosesau';
