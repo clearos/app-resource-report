f98ffd479c06a5341c07eeef60523a39f33ea684
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:55:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Resurs rapporten innehåller information om lasten, minnesanvändning och processer som körs.';
+$lang['resource_report_app_name'] = 'resurs~~POS=TRUNC rapport';
+$lang['resource_report_load_average'] = 'belastning Genomsnitt';
+$lang['resource_report_number_of_processes'] = 'Antal processer';
