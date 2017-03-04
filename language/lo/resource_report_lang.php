bb24d4bf63a008d0a7b93c5ce0b53459eb3498c5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:51:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'ບົດລາຍງານຊັບພະຍາກອນປະກອບມີຂໍ້ມູນກ່ຽວກັບການໂຫຼດ, ການນໍາໃຊ້ຄວາມຊົງຈໍາແລະຂະບວນການແລ່ນ.';
+$lang['resource_report_app_name'] = 'ບົດລາຍງານຊັບພະຍາກອນ';
+$lang['resource_report_load_average'] = 'Load ສະເລ່ຍ';
+$lang['resource_report_number_of_processes'] = 'ຈໍານວນຂອງຂະບວນການ';
