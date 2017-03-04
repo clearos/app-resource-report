1943a8f414b0aaab48fa537ea09258a1ebff4d7d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:40:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'The Resource Report hada bayani a kan load, memory amfani da gudu tafiyar matakai.';
+$lang['resource_report_app_name'] = 'Resource Report';
+$lang['resource_report_load_average'] = 'load Average';
+$lang['resource_report_number_of_processes'] = 'Number of tafiyar matakai';
