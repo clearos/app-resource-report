8ccce05327252f285358019793c51803281cc63e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:34:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Resurssien sisältää tietoa kuorman, muistin käyttö ja käynnissä olevia prosesseja.';
+$lang['resource_report_app_name'] = 'Resource Report';
+$lang['resource_report_load_average'] = 'kuorman keskiarvo';
+$lang['resource_report_number_of_processes'] = 'Useita prosesseja';
