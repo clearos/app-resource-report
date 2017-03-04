7a6942551e54e786061b5220f55a2ab02fdd805f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:53:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Raport zasobów zawiera informacje o obciążeniu, wykorzystanie pamięci i uruchomionych procesów.';
+$lang['resource_report_app_name'] = 'Zgłoś do zasobów';
+$lang['resource_report_load_average'] = 'Średnie obciążenie';
+$lang['resource_report_number_of_processes'] = 'Liczba procesów';
