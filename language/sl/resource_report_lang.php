8a91918c95296625cbcce65092c4092f15fb7ca3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:54:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Poročilo Vir vsebuje podatke o obremenitvi, uporabo pomnilnika in tekočih procesov.';
+$lang['resource_report_app_name'] = 'Poročilo Resource';
+$lang['resource_report_load_average'] = 'obremenitev Povprečje';
+$lang['resource_report_number_of_processes'] = 'Število procesov';
