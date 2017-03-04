a713059c3432da3a3afee39d5636dc5a6fbdafe6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:52:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Resursu ziņojumā iekļauta informācija par slodzi, atmiņas izmantošanu un ekspluatācijas procesiem.';
+$lang['resource_report_app_name'] = 'resursu pārskats';
+$lang['resource_report_load_average'] = 'slodze vidējais';
+$lang['resource_report_number_of_processes'] = 'Skaits procesu';
