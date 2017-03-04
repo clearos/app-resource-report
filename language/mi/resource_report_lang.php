6bcdca823d725130a56e7da7862d14374d5f9be1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:54:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Kei roto te Report Resource mōhiohio i runga i te kawenga, te whakamahinga mahara me tukanga rere.';
+$lang['resource_report_app_name'] = 'Pūrongo rauemi';
+$lang['resource_report_load_average'] = 'Wawaenga Uta';
+$lang['resource_report_number_of_processes'] = 'Tau o tukanga';
