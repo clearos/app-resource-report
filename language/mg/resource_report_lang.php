01a50c78f347086ec062bb88e7a10787b508cc3a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:53:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Ny Resource Report dia ahitana fanazavana momba ny entana, fitadidiana fampiasana sy mihazakazaka dingana.';
+$lang['resource_report_app_name'] = 'Resource Report';
+$lang['resource_report_load_average'] = 'Load Average';
+$lang['resource_report_number_of_processes'] = 'Fomba maro';
