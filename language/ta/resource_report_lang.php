713ed24f8cdf608b84871dbd19213cbe5e4a0355
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:55:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'வள அறிக்கை சுமை, நினைவக பயன்பாடு மற்றும் இயங்கும் பணிகளின் பற்றிய தகவல்களை கொண்டுள்ளது.';
+$lang['resource_report_app_name'] = 'வள அறிக்கை';
+$lang['resource_report_load_average'] = 'சுமை சராசரி';
+$lang['resource_report_number_of_processes'] = 'பணிகளின் எண்ணிக்கை';
