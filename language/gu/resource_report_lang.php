8fd56e0793ad5af97aacd3e60d635a763d97c1b7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:38:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'રિસોર્સ રિપોર્ટ લોડ, મેમરી વપરાશ અને ચાલી રહેલ પ્રક્રિયાઓ પર જાણકારી સમાવે છે.';
+$lang['resource_report_app_name'] = 'રિસોર્સ રિપોર્ટ';
+$lang['resource_report_load_average'] = 'સરેરાશ ભાર';
+$lang['resource_report_number_of_processes'] = 'પ્રક્રિયાઓની સંખ્યા';
