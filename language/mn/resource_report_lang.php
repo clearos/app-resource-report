f1b7a9185b659a0b7bd7d2e16bfd9ed030f194ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:56:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Нөөцийн тайлан ачаалал, санах ойн хэрэглээ болон ажиллаж байгаа процессуудын тухай мэдээллийг агуулдаг.';
+$lang['resource_report_app_name'] = 'нөөцийн тайлан';
+$lang['resource_report_load_average'] = 'ачааллын дундаж';
+$lang['resource_report_number_of_processes'] = 'Үйл явц тоо';
