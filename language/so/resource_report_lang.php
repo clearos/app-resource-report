9e78a00ffad991df7565b21f1fc2395422cf7693
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:54:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Report Resource waxaa ka mid ah warbixin ku saabsan load, isticmaalka xusuusta iyo geedi socodka socda.';
+$lang['resource_report_app_name'] = 'Resource Report';
+$lang['resource_report_load_average'] = 'Celceliska load';
+$lang['resource_report_number_of_processes'] = 'Number of Habsocodsiinta';
