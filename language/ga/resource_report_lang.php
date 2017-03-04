c7be5034798271499f8d727086b23915fd58c326
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:43:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Áirítear ar an Tuarascáil Acmhainne faisnéis ar ualach, úsáid cuimhne agus na próisis a reáchtáil.';
+$lang['resource_report_app_name'] = 'Tuarascáil Acmhainne';
+$lang['resource_report_load_average'] = 'Meán Luchtaigh';
+$lang['resource_report_number_of_processes'] = 'Líon na bPróiseas';
