6481e20f85e265031082d0a830e7ec2a335b2847
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:55:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'రిసోర్స్ నివేదిక లోడ్, మెమరీ వినియోగం మరియు రన్నింగ్ ప్రాసెస్ల సమాచారాన్ని కలిగి ఉంది.';
+$lang['resource_report_app_name'] = 'రిసోర్స్ నివేదిక';
+$lang['resource_report_load_average'] = 'సగటు లోడ్';
+$lang['resource_report_number_of_processes'] = 'ప్రాసెసెస్ సంఖ్య';
