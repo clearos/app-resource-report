202612be2ffc06a6d102ca6d0401bb8c8bfa979c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:54:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Извештајот на ресурси вклучува информации за оптоварување, користењето на меморијата и водење на процеси.';
+$lang['resource_report_app_name'] = 'Пријави ресурси';
+$lang['resource_report_load_average'] = 'Среднаоптовареност';
+$lang['resource_report_number_of_processes'] = 'Број на процеси';
