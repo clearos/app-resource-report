459219cd5548ae667149233bd98978e24f2d707b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:55:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Report Resource e akarelletsa tlhahisoleseding e mabapi le mojaro, tšebeliso memori le dithulaganyo matha.';
+$lang['resource_report_app_name'] = 'Report Resource';
+$lang['resource_report_load_average'] = 'mojaro Karolelano';
+$lang['resource_report_number_of_processes'] = 'Palo ea tšebetso';
