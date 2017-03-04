0b14c23c483a37493dc957677f3e29d0fdd99a45
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:55:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'റിസോഴ്സ് റിപ്പോർട്ട് ലോഡ്, മെമ്മറി ഉപയോഗം പ്രവർത്തിക്കുന്ന പ്രക്രിയകൾ സംബന്ധിച്ചുളള വിവരങ്ങൾ.';
+$lang['resource_report_app_name'] = 'വിഭവ റിപ്പോർട്ട്';
+$lang['resource_report_load_average'] = 'ലോഡ് ശരാശരി';
+$lang['resource_report_number_of_processes'] = 'പ്രക്രിയകളുടെ എണ്ണം';
