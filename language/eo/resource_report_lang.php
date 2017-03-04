25b1429010a978c01b8ead96fef80fe5beea0dcf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:33:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'La Rimedoj Raporti inkluzivas informo sur ŝarĝo, memoro uzado kaj kurante procezoj.';
+$lang['resource_report_app_name'] = 'rimedo Raporti';
+$lang['resource_report_load_average'] = 'ŝarĝo Averaĝa';
+$lang['resource_report_number_of_processes'] = 'Numeron de Procezoj';
