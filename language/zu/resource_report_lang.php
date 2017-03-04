3b58bb3b7b71489909b5c90e0726c7b5ad45f0a1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:57:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Umbiko Resource kuhlanganisa ulwazi umthwalo, kwememori kanye izinqubo egijima.';
+$lang['resource_report_app_name'] = 'Umbiko Resource';
+$lang['resource_report_load_average'] = 'Layisha esijwayelekile';
+$lang['resource_report_number_of_processes'] = 'Number of Izinqubo';
