a5b8b0302e34c000be3378d1c06e323378892f69
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:54:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Správa Resource obsahuje informácie o zaťažení, využitie pamäte a bežiacich procesov.';
+$lang['resource_report_app_name'] = 'správa Resource';
+$lang['resource_report_load_average'] = 'priemerná záťaž';
+$lang['resource_report_number_of_processes'] = 'počet procesov';
