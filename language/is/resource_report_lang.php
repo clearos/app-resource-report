f0901a681d48cb37e99e4450f0906758c1e2546a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:44:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'The Resource Report inniheldur upplýsingar um álag, minni notkun og hlaupandi aðferð.';
+$lang['resource_report_app_name'] = 'Resource Skýrsla';
+$lang['resource_report_load_average'] = 'hlaða Meðaltal';
+$lang['resource_report_number_of_processes'] = 'Fjöldi ferla';
