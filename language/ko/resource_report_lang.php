193520c90d6012b0496b9b321621b7464ee795fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:49:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = '자원 보고서는 부하, 메모리 사용 및 실행중인 프로세스에 대한 정보를 포함한다.';
+$lang['resource_report_app_name'] = '자원 보고서';
+$lang['resource_report_load_average'] = '로드 평균';
+$lang['resource_report_number_of_processes'] = '프로세스의 수';
