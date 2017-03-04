1e94e14ab906f6eca80620c1cddcc4438f5ca549
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:47:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'რესურს ანგარიში მოიცავს ინფორმაციას დატვირთვის, მეხსიერების გამოყენების და გაშვებული პროცესების.';
+$lang['resource_report_app_name'] = 'რესურსების ანგარიში';
+$lang['resource_report_load_average'] = 'Load საშუალო';
+$lang['resource_report_number_of_processes'] = 'პუნქტების პროცესები';
