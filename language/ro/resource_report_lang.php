14a038947ed995b47b4d31b6462137f1a670b93f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:53:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Raportul de Resurse include informații despre sarcină, utilizarea memoriei și a proceselor care rulează.';
+$lang['resource_report_app_name'] = 'Raportul Resurselor';
+$lang['resource_report_load_average'] = 'încărcarea medie';
+$lang['resource_report_number_of_processes'] = 'Numărul proceselor';
