e68c893c5ed43e81cbc4f6ec1e42d748f7d32a61
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:52:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Išteklių ataskaita apima informaciją apie apkrovą, atminties naudojimo ir veiklos procesus.';
+$lang['resource_report_app_name'] = 'išteklių ataskaita';
+$lang['resource_report_load_average'] = 'apkrovos Vidutinis';
+$lang['resource_report_number_of_processes'] = 'Procesų skaičius';
