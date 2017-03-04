4144f2841ca71f5eda796b21a4c2739ab2699de4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:46:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'リソースレポートは、負荷、メモリ使用量と実行中のプロセスに関する情報が含まれています。';
+$lang['resource_report_app_name'] = 'リソースレポート';
+$lang['resource_report_load_average'] = 'ロード平均';
+$lang['resource_report_number_of_processes'] = '工程数';
