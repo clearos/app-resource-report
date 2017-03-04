c64c47a8ada0a7b8f9faffea040bbb8121411e03
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:57:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Laporan Sumber termasuk maklumat mengenai beban, penggunaan memori dan proses berjalan.';
+$lang['resource_report_app_name'] = 'Laporan sumber';
+$lang['resource_report_load_average'] = 'Sederhana beban';
+$lang['resource_report_number_of_processes'] = 'Bilangan Proses';
