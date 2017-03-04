c42e42264c7f84f556cb7f9b734afa8de56425df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:56:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Resurs hisoboti yuk, xotira foydalanish va ishlayotgan jarayonlari haqida ma&#39;lumot o&#39;z ichiga oladi.';
+$lang['resource_report_app_name'] = 'Resurs hisoboti';
+$lang['resource_report_load_average'] = 'yuk O&#39;rtacha';
+$lang['resource_report_number_of_processes'] = 'Jarayonlarni soni';
