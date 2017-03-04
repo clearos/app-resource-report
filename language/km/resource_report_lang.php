f42726dea3d86f4ef415ced15817257ed8bec6c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:48:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'របាយការណ៍ធនធានរួមបញ្ចូលទាំងការអំពីការផ្ទុក, ការប្រើប្រាស់សតិនិងដំណើរការដែលកំពុងរត់។';
+$lang['resource_report_app_name'] = 'របាយការណ៍ធនធាន';
+$lang['resource_report_load_average'] = 'មធ្យមនៃការផ្ទុក';
+$lang['resource_report_number_of_processes'] = 'ចំនួននៃដំណើរការ';
