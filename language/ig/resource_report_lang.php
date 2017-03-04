920d5fa183cda7ca13c915a2252d99a07830775d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 21:43:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'The Resource Report na-agụnye ozi na ibu, ebe nchekwa ojiji na-agba ọsọ usoro.';
+$lang['resource_report_app_name'] = 'Resource Report';
+$lang['resource_report_load_average'] = 'ibu Nkezi';
+$lang['resource_report_number_of_processes'] = 'Number nke usoro';
