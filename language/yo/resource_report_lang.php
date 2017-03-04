bf19cad9701bb17b6bbdda4136dca1182edfb0db
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:57:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'The Resource Iroyin pẹlu alaye lori fifuye, iranti lilo ati ki o nṣiṣẹ sii lakọkọ.';
+$lang['resource_report_app_name'] = 'Resource Iroyin';
+$lang['resource_report_load_average'] = 'fifuye Apapọ';
+$lang['resource_report_number_of_processes'] = 'Nọmba ti lakọkọ';
