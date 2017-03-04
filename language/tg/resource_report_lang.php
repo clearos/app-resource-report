d9f0743916ea861f383b74e7399a5773e5572ff2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:56:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Дар Њисобот оид ба захираҳо дорои маълумот дар бораи сарбории, истифода хотира ва равандҳои иҷро.';
+$lang['resource_report_app_name'] = 'Њисобот оид ба захираҳо';
+$lang['resource_report_load_average'] = 'Меъёри биншон';
+$lang['resource_report_number_of_processes'] = 'Миқдори равандҳое,';
