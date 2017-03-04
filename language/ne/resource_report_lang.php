68885dc814cf1e9d52151e1006e2e2e92c17125d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:52:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'स्रोत रिपोर्ट लोड, स्मृति उपयोग र चलिरहेको प्रक्रियाहरूको जानकारी पनि समावेश छ।';
+$lang['resource_report_app_name'] = 'स्रोत रिपोर्ट';
+$lang['resource_report_load_average'] = 'औसत लोड';
+$lang['resource_report_number_of_processes'] = 'प्रक्रियाहरू संख्या';
