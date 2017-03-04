b7dd6904af18db5e8cc19edc5ed27fd38a728c41
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:56:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'وسائل رپورٹ لوڈ، میموری استعمال اور چلانے کے عمل کے بارے میں معلومات بھی شامل ہے.';
+$lang['resource_report_app_name'] = 'وسائل رپورٹ';
+$lang['resource_report_load_average'] = 'لوڈ اوسط';
+$lang['resource_report_number_of_processes'] = 'عمل کی تعداد';
