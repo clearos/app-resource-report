3fbffd791897017cc42e62c68f9e1b448ecd1d91
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:57:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'די מיטל באריכט כולל אינפֿאָרמאַציע אויף מאַסע, זיקאָרן באַניץ און פליסנדיק פּראַסעסאַז.';
+$lang['resource_report_app_name'] = 'ריסאָרס באריכט';
+$lang['resource_report_load_average'] = 'מאַסע דורכשניטלעך';
+$lang['resource_report_number_of_processes'] = 'נומער פון פּראָסעססעס';
