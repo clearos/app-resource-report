913b6d685ca3dd7b75dc12a5e0a794c2ae4cb8f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-04 23:57:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/resource_report_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['resource_report_app_description'] = 'Báo cáo tài nguyên bao gồm các thông tin về tải trọng, sử dụng bộ nhớ và tiến trình đang chạy.';
+$lang['resource_report_app_name'] = 'báo cáo tài nguyên';
+$lang['resource_report_load_average'] = 'Tải trung bình';
+$lang['resource_report_number_of_processes'] = 'Số Processes';
